199733738b1c3e8343493033e148030612232447
ABOOD7333 <abood7333@github.com>
2026-05-31 03:41:33 +0300

    Add CORSMiddleware to allow cross-origin requests from localhost (Chrome web)

--- main.py
+++ main.py
@@ -134,6 +134,15 @@ class LoginRateLimitMiddleware(BaseHTTPMiddleware):
 
 app.add_middleware(LoginRateLimitMiddleware)
 
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|.*\.up\.railway\.app)(:\d+)?",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
